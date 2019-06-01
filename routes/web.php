b9657292872532777a20bb806249e14cc8e55067
onyekachila <onyekachila@gmail.com>
2019-06-01 12:48:40 +0100

    Added authentication for user and admin

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+// User Routes
+Route::group(['namespace' => 'User'],function(){
+	Route::get('/','HomeController@index');
+
 });
+
+//Admin Routes
+Route::group(['namespace' => 'Admin'],function(){
+	Route::get('admin/home','HomeController@index')->name('admin.home');
+	// Users Routes
+	Route::resource('admin/user','UserController');
+	// Roles Routes
+	Route::resource('admin/role','RoleController');
+	// Permission Routes
+	Route::resource('admin/permission','PermissionController');
+	// Post Routes
+	Route::resource('admin/post','PostController');
+	// Tag Routes
+	Route::resource('admin/tag','TagController');
+	// Category Routes
+	Route::resource('admin/category','CategoryController');
+	// Admin Auth Routes
+	Route::get('admin-login', 'Auth\LoginController@showLoginForm')->name('admin.login');
+	Route::post('admin-login', 'Auth\LoginController@login');
+});
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/admin/home', function () {
-    return view('admin/home');
-});
